3f660a92976a06952b46bb0e96ee48abea7ecff1
andrezinho <110039135+AndreKaled@users.noreply.github.com>
2022-10-06 18:05:21 -0400

    Add files via upload

---
+++ Biblioteca/src/modelo/Funcionario.java
@@ -0,0 +1,12 @@
+package modelo;
+
+import java.util.List;
+
+public class Funcionario {
+	String nome, numOAb, matricula;
+	int totalEmprestimos,diaEmpestimo;
+	List<Livro> livrosEmprestados;
+	
+	
+	
+}
